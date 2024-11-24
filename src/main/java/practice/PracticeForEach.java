fa9f374859b245924f040039d03ba83d9569bca2
LAPTOP-LIQEF71F\k-kan <k-kawai@seassist.co.jp>
2024-11-24 18:23:16 +0900

    first commit(Basic Grammar task1-6)

---
+++ src/main/java/practice/PracticeForEach.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class PracticeForEach {
+
+	public static void main(String[] args) {
+		
+		String[] names = { "田原", "福尾", "倉成", "濱野", "前村", "重永", "伊藤", "佐藤", "前田", "勝田" };
+		
+		for (String name : names) {
+			System.out.println(name);
+		}
+
+	}
+
+}
---
+++ src/main/java/practice/PracticeSwitch.java
@@ -0,0 +1,42 @@
+package practice;
+
+import java.util.Random;
+
+public class PracticeSwitch {
+
+	public static void main(String[] args) {
+
+		int[] num = new int[10];
+		num[0] = 1;
+		num[1] = 2;
+		num[2] = 3;
+		num[3] = 4;
+		num[4] = 5;
+		
+		Random r = new Random();
+		
+		for (int i = 1; i <= 10; i++) {
+			
+			int rank = num[r.nextInt(4)];
+			
+			switch (rank) {
+			case 1:
+				System.out.println(rank + ": " + "勇者");
+				break;
+			case 2:
+				System.out.println(rank + ": " + "バトルマスター");
+				break;
+			case 3:
+				System.out.println(rank + ": " + "パラディン");
+				break;
+			case 4:
+				System.out.println(rank + ": " + "海賊");
+				break;
+			case 5:
+				System.out.println(rank + ": " + "魔法剣士");
+				break;
+			}
+		}
+	}
+
+}
